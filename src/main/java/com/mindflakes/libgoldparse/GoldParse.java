56381d7d360e445984181ebc20945ca8fb90af2a
Nelson Chen <crazysim@gmail.com>
2010-07-20 00:15:45 -0700

    added jsoup and little code

--- src/main/java/com/mindflakes/libgoldparse/GoldParse.java
+++ src/main/java/com/mindflakes/libgoldparse/GoldParse.java
@@ -5,7 +5,11 @@ package com.mindflakes.libgoldparse;
  * User: nelson
  * Date: Jul 20, 2010
  * Time: 12:02:42 AM
- * To change this template use File | Settings | File Templates.
  */
 public class GoldParse {
+    private GoldParse(){}
+
+    public static GoldSchedule parse() {
+        return new GoldSchedule();  
+    }
 }
---
+++ src/main/java/com/mindflakes/libgoldparse/GoldSchedule.java
@@ -0,0 +1,14 @@
+package com.mindflakes.libgoldparse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nelson
+ * Date: Jul 20, 2010
+ * Time: 12:12:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GoldSchedule {
+    public GoldSchedule(){
+        
+    }
+}
