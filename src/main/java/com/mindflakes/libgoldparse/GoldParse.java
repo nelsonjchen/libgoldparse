51bc5b8f7289285872b00056d5714a80a960aaad
Nelson Chen <crazysim@gmail.com>
2010-07-20 00:05:08 -0700

    new direction inspired by jsoup

---
+++ src/main/java/com/mindflakes/libgoldparse/GoldParse.java
@@ -0,0 +1,11 @@
+package com.mindflakes.libgoldparse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nelson
+ * Date: Jul 20, 2010
+ * Time: 12:02:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GoldParse {
+}
