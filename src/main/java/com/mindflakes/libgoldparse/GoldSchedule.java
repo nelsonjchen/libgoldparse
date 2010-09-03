eb1ebfdd12d11995724e0b246e1407af5c872c2e
Nelson Chen <crazysim@gmail.com>
2010-09-02 19:02:18 -0700

    added basic schedule class

--- src/main/java/com/mindflakes/libgoldparse/GoldSchedule.java
+++ src/main/java/com/mindflakes/libgoldparse/GoldSchedule.java
@@ -1,5 +1,7 @@
 package com.mindflakes.libgoldparse;
 
+import java.util.ArrayList;
+
 /**
  * Created by IntelliJ IDEA.
  * User: nelson
@@ -8,7 +10,40 @@ package com.mindflakes.libgoldparse;
  * To change this template use File | Settings | File Templates.
  */
 public class GoldSchedule {
+	
+	private ArrayList<GoldClass> classes;
+	private String quarter;
+	private int year;
+	
     public GoldSchedule(){
-        
+    	classes = new ArrayList<GoldClass>();
     }
+    
+    public void addClass(GoldClass goldclass){
+    	if (classes.contains(goldclass)){
+    		return;
+    	}
+    	classes.add(goldclass);
+    }
+    
+    public void removeClass(GoldClass goldclass){
+    	classes.remove(goldclass);
+    }
+    
+    public String getQuarter() {
+		return quarter;
+	}
+
+	public void setQuarter(String quarter) {
+		this.quarter = quarter;
+	}
+	
+	public int getYear(){
+		return year;
+	}
+	
+	public void setYear(int year){
+		this.year = year;
+	}
+	
 }
