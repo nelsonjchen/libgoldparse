a5299c55ce836195f124fa30bec195d799b89870
Nelson Chen <crazysim@gmail.com>
2010-10-06 20:08:09 -0700

    session edit

--- src/main/java/com/mindflakes/libgoldparse/GoldClassSession.java
+++ src/main/java/com/mindflakes/libgoldparse/GoldClassSession.java
@@ -2,9 +2,28 @@ package com.mindflakes.libgoldparse;
 
 public class GoldClassSession {
 	
+	private String lecturer;
+	private String room;
 	
-	public String toString(){
-		return "hi";
+	public String getLecturer() {
+		return lecturer;
+	}
+	
+	public void setLecturer(String lecturer) {
+		this.lecturer = lecturer;
+	}
+	
+	public String getRoom() {
+		return room;
+	}
+	
+	public void setRoom(String room) {
+		this.room = room;
+	}
+	
+	public boolean isDiscussion(){
+		//TODO: fix
+		return true;
 	}
 
 }
